94673deb58d2e8a9f0baa3f114a861ed3262531b
Mykhailo Seniutovych <Mykhailo.Seniutovych@Eleks.com>
2021-07-04 18:01:58 +0300

    Search field on mobile devices

--- VideoHub.WebClient/src/app/shared/header/header.component.ts
+++ VideoHub.WebClient/src/app/shared/header/header.component.ts
@@ -6,21 +6,28 @@ import { SearchService } from "./../services/search.service";
     selector: "app-header",
     templateUrl: "./header.component.html",
     styleUrls: ["./header.component.scss"],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+    changeDetection: ChangeDetectionStrategy.Default,
 })
 export class HeaderComponent {
     @ViewChild("searchInput") searchInput: ElementRef;
 
-    isSearchActivated = false;
     user$ = this.authService.user$;
+    searchValue = "";
+
+    private isSearchActivated = false;
+    private isSearchTextFilled = false;
 
     constructor(
         private readonly authService: AuthService,
         private readonly searchService: SearchService) {
     }
 
-    @HostBinding("class.search-activated") get valid(): boolean {
+    @HostBinding("class.search-activated") get searchActivated(): boolean {
         return this.isSearchActivated;
+    }
+
+    @HostBinding("class.search-text-filled") get searchFilled(): boolean {
+        return this.isSearchTextFilled;
     }
 
     async logout(): Promise<void> {
@@ -31,8 +38,30 @@ export class HeaderComponent {
         this.searchService.setValue(value);
     }
 
-    toggleSearch(): void {
+    onSearchClicked(): void {
+        if (this.isSearchActivated) {
+            this.search(this.searchValue);
+        } else {
+            setTimeout(() => this.searchInput.nativeElement.focus());
+        }
+
         this.isSearchActivated = !this.isSearchActivated;
-        setTimeout(() => this.searchInput.nativeElement.focus());
+        this.searchValue = "";
+        this.isSearchTextFilled = false;
+    }
+
+    onInputChange(event: any): void {
+        this.searchValue = event.target.value;
+        this.isSearchTextFilled = !!this.searchValue;
+    }
+
+    onClearClicked(): void {
+        this.searchValue = "";
+        setTimeout(() => this.isSearchTextFilled = false, 100);
+    }
+
+    onBackClicked(): void {
+        this.isSearchActivated = false;
+        this.isSearchTextFilled = false;
     }
 }
