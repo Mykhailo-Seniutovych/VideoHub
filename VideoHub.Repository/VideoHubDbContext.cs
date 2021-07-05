2c86d9219f5d1eeba932b0646136c3b57866970a
Mykhailo Seniutovych <Mykhailo.Seniutovych@Eleks.com>
2021-07-05 08:09:11 +0300

    Add channels

--- VideoHub.Repository/VideoHubDbContext.cs
+++ VideoHub.Repository/VideoHubDbContext.cs
@@ -31,7 +31,7 @@ namespace VideoHub.Repository
 
             // Use this line when you create a new DB migration.
             // The migration will connect from a host machine into a docker container using this connection string
-            // optionsBuilder.UseNpgsql("User ID=postgres;Password=admin;Host=localhost;Port=5433;Database=VideoHub;Pooling=true;Minimum Pool Size=0;Maximum Pool Size=100;Connection Lifetime=0;");
+            //optionsBuilder.UseNpgsql("User ID=postgres;Password=admin;Host=localhost;Port=5433;Database=VideoHub;Pooling=true;Minimum Pool Size=0;Maximum Pool Size=100;Connection Lifetime=0;");
         }
     }
 }
